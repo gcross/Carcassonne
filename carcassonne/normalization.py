acaefad76c8a593b3352812e9a39ebccda6f763e
Gregory Crosswhite <gcrosswhite@gmail.com>
2012-08-20 15:06:28 +1000

    Wrote tested code to compute the normalization.

---
+++ carcassonne/normalization.py
@@ -0,0 +1,26 @@
+# Imports {{{
+from .utils import FromRight, FromBoth, formAbsorber, formContractor
+# }}}
+
+# Classes {{{
+class Normalization(): # {{{
+    def __init__(self,corners,sides): # {{{
+        self.corners = list(corners)
+        self.sides = list(sides)
+    # }}}
+    def absorbCenter(self,direction,center): # {{{
+        self.corners[direction] = self.corners[direction].absorbFromLeft(self.sides[(direction+1)%4])
+        self.sides[direction] = self.sides[direction].absorbCenter(center)
+        self.corners[(direction-1)%4] = self.corners[(direction-1)%4].absorbFromRight(self.sides[(direction-1)%4])
+    # }}}
+    def formMultiplier(self): # {{{
+        return self.sides[0].formMultiplier(self.corners,self.sides)
+    # }}}
+# }}}
+# }}}
+
+# Exports {{{
+__all__ = [
+    "Normalization",
+]
+# }}}
---
+++ carcassonne/tests/test_normalization.py
@@ -0,0 +1,111 @@
+# Imports {{{
+from copy import copy
+from random import randint
+
+from . import *
+from ..data import *
+from ..normalization import *
+from ..tensors.normalization import *
+# }}}
+
+class TestNormalization(TestCase): # {{{
+    @staticmethod # generateData # {{{
+    def generateData():
+        n = 5
+        corners_data = tuple(NDArrayData.newRandom(*(randint(1,n),)*2) for _ in range(4))
+        center_data = NDArrayData.newRandom(*(randint(1,n),)*5)
+        sides_data = tuple(NDArrayData.newRandom(corners_data[i].shape[1],corners_data[(i-1)%4].shape[0],center_data.shape[i],center_data.shape[i]) for i in range(4))
+        corners = tuple(map(NormalizationCorner,corners_data))
+        sides = tuple(NormalizationSide(i,sides_data[i]) for i in range(4))
+        return corners_data, sides_data, center_data, corners, sides
+    # }}}
+    @with_checker
+    def test_absorbCenter_0(self): # {{{
+        corners_data, sides_data, center_data, corners, sides = self.generateData()
+        normalization = Normalization(corners,sides)
+        normalization.absorbCenter(0,center_data)
+        corners = list(corners)
+        sides = list(sides)
+        corners[0] = corners[0].absorbFromLeft(sides[1])
+        sides[0] = sides[0].absorbCenter(center_data)
+        corners[-1] = corners[-1].absorbFromRight(sides[-1])
+        self.assertDataAlmostEqual(normalization.corners[0].data,corners[0].data)
+        self.assertDataAlmostEqual(normalization.corners[1].data,corners[1].data)
+        self.assertDataAlmostEqual(normalization.corners[2].data,corners[2].data)
+        self.assertDataAlmostEqual(normalization.corners[3].data,corners[3].data)
+        self.assertDataAlmostEqual(normalization.sides[0].data,sides[0].data)
+        self.assertDataAlmostEqual(normalization.sides[1].data,sides[1].data)
+        self.assertDataAlmostEqual(normalization.sides[2].data,sides[2].data)
+        self.assertDataAlmostEqual(normalization.sides[3].data,sides[3].data)
+    # }}}
+    @with_checker
+    def test_absorbCenter_1(self): # {{{
+        corners_data, sides_data, center_data, corners, sides = self.generateData()
+        normalization = Normalization(corners,sides)
+        normalization.absorbCenter(1,center_data)
+        corners = list(corners)
+        sides = list(sides)
+        corners[1] = corners[1].absorbFromLeft(sides[2])
+        sides[1] = sides[1].absorbCenter(center_data)
+        corners[0] = corners[0].absorbFromRight(sides[0])
+        self.assertDataAlmostEqual(normalization.corners[0].data,corners[0].data)
+        self.assertDataAlmostEqual(normalization.corners[1].data,corners[1].data)
+        self.assertDataAlmostEqual(normalization.corners[2].data,corners[2].data)
+        self.assertDataAlmostEqual(normalization.corners[3].data,corners[3].data)
+        self.assertDataAlmostEqual(normalization.sides[0].data,sides[0].data)
+        self.assertDataAlmostEqual(normalization.sides[1].data,sides[1].data)
+        self.assertDataAlmostEqual(normalization.sides[2].data,sides[2].data)
+        self.assertDataAlmostEqual(normalization.sides[3].data,sides[3].data)
+    # }}}
+    @with_checker
+    def test_absorbCenter_2(self): # {{{
+        corners_data, sides_data, center_data, corners, sides = self.generateData()
+        normalization = Normalization(corners,sides)
+        normalization.absorbCenter(2,center_data)
+        corners = list(corners)
+        sides = list(sides)
+        corners[2] = corners[2].absorbFromLeft(sides[3])
+        sides[2] = sides[2].absorbCenter(center_data)
+        corners[1] = corners[1].absorbFromRight(sides[1])
+        self.assertDataAlmostEqual(normalization.corners[0].data,corners[0].data)
+        self.assertDataAlmostEqual(normalization.corners[1].data,corners[1].data)
+        self.assertDataAlmostEqual(normalization.corners[2].data,corners[2].data)
+        self.assertDataAlmostEqual(normalization.corners[3].data,corners[3].data)
+        self.assertDataAlmostEqual(normalization.sides[0].data,sides[0].data)
+        self.assertDataAlmostEqual(normalization.sides[1].data,sides[1].data)
+        self.assertDataAlmostEqual(normalization.sides[2].data,sides[2].data)
+        self.assertDataAlmostEqual(normalization.sides[3].data,sides[3].data)
+    # }}}
+    @with_checker
+    def test_absorbCenter_3(self): # {{{
+        corners_data, sides_data, center_data, corners, sides = self.generateData()
+        normalization = Normalization(corners,sides)
+        normalization.absorbCenter(3,center_data)
+        corners = list(corners)
+        sides = list(sides)
+        corners[3] = corners[3].absorbFromLeft(sides[0])
+        sides[3] = sides[3].absorbCenter(center_data)
+        corners[2] = corners[2].absorbFromRight(sides[2])
+        self.assertDataAlmostEqual(normalization.corners[0].data,corners[0].data)
+        self.assertDataAlmostEqual(normalization.corners[1].data,corners[1].data)
+        self.assertDataAlmostEqual(normalization.corners[2].data,corners[2].data)
+        self.assertDataAlmostEqual(normalization.corners[3].data,corners[3].data)
+        self.assertDataAlmostEqual(normalization.sides[0].data,sides[0].data)
+        self.assertDataAlmostEqual(normalization.sides[1].data,sides[1].data)
+        self.assertDataAlmostEqual(normalization.sides[2].data,sides[2].data)
+        self.assertDataAlmostEqual(normalization.sides[3].data,sides[3].data)
+    # }}}
+    @with_checker
+    def test_formMultiplier(self): # {{{
+        corners_data, sides_data, center_data, corners, sides = self.generateData()
+
+        observed = Normalization(corners,sides).formMultiplier()(center_data)
+        exact = formDataContractor(
+            [Join(i,1,i+4,0) for i in range(4)]+
+            [Join(i,0,(i+1)%4+4,1) for i in range(4)]+
+            [Join(i+4,2,8,i) for i in range(4)],
+            [[(i+4,3)] for i in range(4)]+[[(8,4)]]
+        )(*corners_data + sides_data + (center_data,))
+        self.assertDataAlmostEqual(observed,exact)
+    # }}}
+# }}}
