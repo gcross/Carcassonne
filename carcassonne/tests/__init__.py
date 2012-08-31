95d4b7e73c0cecd4f29436c2f98b939d2562e293
Gregory Crosswhite <gcrosswhite@gmail.com>
2012-08-31 10:47:46 +1000

    Cosmetic fix:  deleted whitespace in tests/__init__.py to match project style

--- carcassonne/tests/__init__.py
+++ carcassonne/tests/__init__.py
@@ -13,7 +13,6 @@ from ..utils import *
 # }}}
 
 # Decorators {{{
-
 class prependContractor: # {{{
     def __init__(self,*args,**keywords):
         self.contractor = formContractor(*args,**keywords)
@@ -22,11 +21,9 @@ class prependContractor: # {{{
             return f(self.contractor,*args,**keywords)
         return new_f
 # }}}
-
 # }}}
 
 # Functions {{{
-
 def ensurePhysicalDimensionSufficientlyLarge(tensor,index,dimension): # {{{
     if dimension > product(withoutIndex(tensor.dimensions(),index)):
         new_shape = list(tensor.dimensions())
@@ -35,72 +32,58 @@ def ensurePhysicalDimensionSufficientlyLarge(tensor,index,dimension): # {{{
     else:
         return tensor
 # }}}
-
 def ensurePhysicalDimensionSufficientlyLargeToNormalize(tensor,index): # {{{
     return ensurePhysicalDimensionSufficientlyLarge(tensor,index,tensor.dimension(index))
 # }}}
-
 def randomIndex(ndim): # {{{
     return randint(0,ndim-1)
 # }}}
-
 def randomNormalizableTensorAndIndex(ndim): # {{{
     index = randomIndex(ndim)
     shape = randomShape(ndim)
     shape[index] = randint(1,product(withoutIndex(shape,index)))
     return crand(*shape), index
 # }}}
-
 def randomPermutation(size): # {{{
     permutation = list(range(size))
     shuffle(permutation)
     return permutation
 # }}}
-
 def randomShape(ndim,maximum=5): # {{{
     return tuple(randint(1,maximum) for _ in range(ndim))
 # }}}
-
 def randomShapeAgreeingWith(ndim,index,other_dimension): # {{{
     shape = randomShape(ndim)
     shape[index] = other_dimension
     return shape
 # }}}
-
 def randomShuffledPartition(elements): # {{{
     elements = list(elements)
     shuffle(elements)
     return randomPartition(elements)
 # }}}
-
 def randomTensor(ndim): # {{{
     return crand(*randomShape(ndim))
 # }}}
-
 def randomTensorAgreeingWith(ndim,index,other_dimension): # {{{
     return crand(*randomShapeAgreeingWith(ndim,index,other_dimension))
 # }}}
-
 def randomTensorAndIndex(ndim): # {{{
     return randomTensor(ndim), randomIndex(ndim)
 # }}}
-
 def randomTensorAndIndexAgreeingWith(ndim,other_dimension): # {{{
     index = randomIndex(ndim)
     return randomTensorAgreeingWith(ndim,index,other_dimension), index
 
 # }}}
-
 # }}}
 
 # Classes {{{
-
 class Dummy: # {{{
     def __init__(self,**fields):
         for name, value in fields.items():
             setattr(self,name,value)
 # }}}
-
 class TestCase(unittest.TestCase): # {{{
     def assertAllClose(self,v1,v2): # {{{
         v1 = array(v1)
@@ -147,5 +130,4 @@ class TestCase(unittest.TestCase): # {{{
         self.assertAlmostEqual(norm(v),0)
     # }}}
 # }}}
-
 # }}}
