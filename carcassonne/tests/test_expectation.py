1f25d199fa3569c4125c33e9f9108c5f9dce0d19
Gregory Crosswhite <gcrosswhite@gmail.com>
2012-08-31 17:17:23 +1000

    Added some very basic tests using a magnetic field.

--- carcassonne/tests/test_expectation.py
+++ carcassonne/tests/test_expectation.py
@@ -1,4 +1,6 @@
 # Imports {{{
+from numpy import array
+
 from . import *
 from ..expectation import Expectation
 from ..sparse import Identity, Operator
@@ -58,5 +60,28 @@ class TestExpectation(TestCase): # {{{
             else:
                 height += 1
         self.assertEqual(expectation.computeExpectation(state_center_data),width*height)
+    # }}}
+    @staticmethod
+    def makeMagneticField(): # {{{
+        corners = [{Identity():NDArrayData.newTrivial((1,1))}]*4
+        sides = [{Identity():NDArrayData.newTrivial((1,1,1,1))}]*4
+        Z = NDArrayData(array([[1,0],[0,-1]]))
+        operator_center_tensor = {Identity():None,Operator():Z}
+        state_up = NDArrayData(array([[[[[1,0]]]]]))
+        state_down = NDArrayData(array([[[[[0,1]]]]]))
+        return Expectation(corners, sides, operator_center_tensor), [state_up, state_down], [1,-1]
+    # }}}
+    @with_checker(number_of_calls=10)
+    def test_silly_field_single_step(self,direction=irange(0,3),s1=irange(0,1),s2=irange(0,1)): # {{{
+        expectation, states, spins = self.makeMagneticField()
+        expectation.absorbCenter(direction,states[s1])
+        self.assertEqual(expectation.computeExpectation(states[s2]),spins[s1]+spins[s2])
+    # }}}
+    @with_checker(number_of_calls=10)
+    def test_silly_field_double_step_same_direction(self,direction=irange(0,3),s1=irange(0,1),s2=irange(0,1),s3=irange(0,1)): # {{{
+        expectation, states, spins = self.makeMagneticField()
+        expectation.absorbCenter(direction,states[s1])
+        expectation.absorbCenter(direction,states[s2])
+        self.assertEqual(expectation.computeExpectation(states[s3]),spins[s1]+spins[s2]+spins[s3])
     # }}}
 # }}}
