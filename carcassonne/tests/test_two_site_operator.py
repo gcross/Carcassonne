9302aafb34186ccce7377f6fe41d5488f8b6f21a
Gregory Crosswhite <gcrosswhite@gmail.com>
2012-09-17 20:56:39 +1000

    Wrote tests of taking two steps in each direction.

--- carcassonne/tests/test_two_site_operator.py
+++ carcassonne/tests/test_two_site_operator.py
@@ -130,3 +130,107 @@ class TestTwoSiteOperator(TestCase):
 
         self.assertAlmostEqual(system.computeExpectation(),correct_expectation)
     # }}}
+    @with_checker # def test_LR_two_steps_right {{{
+    def test_LR_two_steps_right(self,physical_dimension=irange(1,5)):
+        OO_LR = [NDArrayData.newRandomHermitian(physical_dimension,physical_dimension) for _ in range(2)]
+        OO_L, OO_R = OO_LR
+        system = System.newTrivialWithSparseOperator(OO_LR=OO_LR)
+        Ss = [NDArrayData.newNormalizedRandom(1,1,1,1,physical_dimension) for _ in range(3)]
+        SL, SM, SR = Ss
+        system.setStateCenter(SR)
+        self.assertAlmostEqual(system.computeNormalization(),1)
+        system.absorbCenter(0)
+        system.setStateCenter(SM)
+        self.assertAlmostEqual(system.computeNormalization(),1)
+        system.absorbCenter(0)
+        system.setStateCenter(SL)
+        self.assertAlmostEqual(system.computeNormalization(),1)
+
+        E1L = OO_L.contractWith(SL.ravel(),(1,),(0,)).contractWith(SL.ravel().conj(),(0,),(0,)).extractScalar()
+        E1M = OO_R.contractWith(SM.ravel(),(1,),(0,)).contractWith(SM.ravel().conj(),(0,),(0,)).extractScalar()
+        E1 = E1L*E1M
+
+        E2M = OO_L.contractWith(SM.ravel(),(1,),(0,)).contractWith(SM.ravel().conj(),(0,),(0,)).extractScalar()
+        E2R = OO_R.contractWith(SR.ravel(),(1,),(0,)).contractWith(SR.ravel().conj(),(0,),(0,)).extractScalar()
+        E2 = E2M*E2R
+
+        self.assertAlmostEqual(system.computeExpectation(),E1+E2)
+    # }}}
+    @with_checker # def test_LR_two_steps_left {{{
+    def test_LR_two_steps_left(self,physical_dimension=irange(1,5)):
+        OO_LR = [NDArrayData.newRandomHermitian(physical_dimension,physical_dimension) for _ in range(2)]
+        OO_L, OO_R = OO_LR
+        system = System.newTrivialWithSparseOperator(OO_LR=OO_LR)
+        Ss = [NDArrayData.newNormalizedRandom(1,1,1,1,physical_dimension) for _ in range(3)]
+        SL, SM, SR = Ss
+        system.setStateCenter(SL)
+        self.assertAlmostEqual(system.computeNormalization(),1)
+        system.absorbCenter(2)
+        system.setStateCenter(SM)
+        self.assertAlmostEqual(system.computeNormalization(),1)
+        system.absorbCenter(2)
+        system.setStateCenter(SR)
+        self.assertAlmostEqual(system.computeNormalization(),1)
+
+        E1L = OO_L.contractWith(SL.ravel(),(1,),(0,)).contractWith(SL.ravel().conj(),(0,),(0,)).extractScalar()
+        E1M = OO_R.contractWith(SM.ravel(),(1,),(0,)).contractWith(SM.ravel().conj(),(0,),(0,)).extractScalar()
+        E1 = E1L*E1M
+
+        E2M = OO_L.contractWith(SM.ravel(),(1,),(0,)).contractWith(SM.ravel().conj(),(0,),(0,)).extractScalar()
+        E2R = OO_R.contractWith(SR.ravel(),(1,),(0,)).contractWith(SR.ravel().conj(),(0,),(0,)).extractScalar()
+        E2 = E2M*E2R
+
+        self.assertAlmostEqual(system.computeExpectation(),E1+E2)
+    # }}}
+    @with_checker # def test_UD_two_steps_up {{{
+    def test_UD_two_steps_up(self,physical_dimension=irange(1,5)):
+        OO_UD = [NDArrayData.newRandomHermitian(physical_dimension,physical_dimension) for _ in range(2)]
+        OO_U, OO_D = OO_UD
+        system = System.newTrivialWithSparseOperator(OO_UD=OO_UD)
+        Ss = [NDArrayData.newNormalizedRandom(1,1,1,1,physical_dimension) for _ in range(3)]
+        SU, SM, SD = Ss
+        system.setStateCenter(SU)
+        self.assertAlmostEqual(system.computeNormalization(),1)
+        system.absorbCenter(1)
+        system.setStateCenter(SM)
+        self.assertAlmostEqual(system.computeNormalization(),1)
+        system.absorbCenter(1)
+        system.setStateCenter(SD)
+        self.assertAlmostEqual(system.computeNormalization(),1)
+
+        E1U = OO_U.contractWith(SU.ravel(),(1,),(0,)).contractWith(SU.ravel().conj(),(0,),(0,)).extractScalar()
+        E1M = OO_D.contractWith(SM.ravel(),(1,),(0,)).contractWith(SM.ravel().conj(),(0,),(0,)).extractScalar()
+        E1 = E1U*E1M
+
+        E2M = OO_U.contractWith(SM.ravel(),(1,),(0,)).contractWith(SM.ravel().conj(),(0,),(0,)).extractScalar()
+        E2D = OO_D.contractWith(SD.ravel(),(1,),(0,)).contractWith(SD.ravel().conj(),(0,),(0,)).extractScalar()
+        E2 = E2M*E2D
+
+        self.assertAlmostEqual(system.computeExpectation(),E1+E2)
+    # }}}
+    @with_checker # def test_UD_two_steps_down {{{
+    def test_UD_two_steps_down(self,physical_dimension=irange(1,5)):
+        OO_UD = [NDArrayData.newRandomHermitian(physical_dimension,physical_dimension) for _ in range(2)]
+        OO_U, OO_D = OO_UD
+        system = System.newTrivialWithSparseOperator(OO_UD=OO_UD)
+        Ss = [NDArrayData.newNormalizedRandom(1,1,1,1,physical_dimension) for _ in range(3)]
+        SU, SM, SD = Ss
+        system.setStateCenter(SD)
+        self.assertAlmostEqual(system.computeNormalization(),1)
+        system.absorbCenter(3)
+        system.setStateCenter(SM)
+        self.assertAlmostEqual(system.computeNormalization(),1)
+        system.absorbCenter(3)
+        system.setStateCenter(SU)
+        self.assertAlmostEqual(system.computeNormalization(),1)
+
+        E1U = OO_U.contractWith(SU.ravel(),(1,),(0,)).contractWith(SU.ravel().conj(),(0,),(0,)).extractScalar()
+        E1M = OO_D.contractWith(SM.ravel(),(1,),(0,)).contractWith(SM.ravel().conj(),(0,),(0,)).extractScalar()
+        E1 = E1U*E1M
+
+        E2M = OO_U.contractWith(SM.ravel(),(1,),(0,)).contractWith(SM.ravel().conj(),(0,),(0,)).extractScalar()
+        E2D = OO_D.contractWith(SD.ravel(),(1,),(0,)).contractWith(SD.ravel().conj(),(0,),(0,)).extractScalar()
+        E2 = E2M*E2D
+
+        self.assertAlmostEqual(system.computeExpectation(),E1+E2)
+    # }}}
