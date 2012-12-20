2fb649725adffc1d2b8fe88e4123dcd1414a4639
Gregory Crosswhite <gcrosswhite@gmail.com>
2012-12-20 18:50:34 +1000

    More tests!

--- carcassonne/tests/test_system_1d.py
+++ carcassonne/tests/test_system_1d.py
@@ -26,6 +26,26 @@ class TestSystem1D(TestCase): # {{{
             )
         self.assertAlmostEqual(system.computeOneSiteExpectation(),abs(field_strength))
     # }}}
+    @with_checker # test_computeOneSiteExpectation_Z_field_all_random {{{
+    def test_computeOneSiteExpectation_Z_field_random(self,field_strength=float):
+        system = \
+            System(
+                [1,0],
+                [0,1],
+                buildTensor((2,2,2,2),{
+                    (0,0): Pauli.I,
+                    (0,1): field_strength*Pauli.Z,
+                    (1,1): Pauli.I,
+                }),
+                normalize(crand(2,2,2),1),
+                [1,1],
+                [1,1],
+            )
+        for i in range(100):
+            system.contractLeft()
+            system.contractRight()
+        self.assertAlmostEqual(system.computeOneSiteExpectation(),system.computeExpectation()/201)
+    # }}}
     @with_checker # test_computeOneSiteExpectation_ZZ_field_all_up {{{
     def test_computeOneSiteExpectation_ZZ_field_all_up(self,phase=float,field_strength=float):
         system = \
@@ -58,6 +78,23 @@ class TestSystem1D(TestCase): # {{{
                 array([[[cos(phase)+1j*sin(phase),0]]]),
             )
         self.assertAlmostEqual(system.computeOneSiteExpectation(),abs(field_strength))
+    # }}}
+    @with_checker # test_computeOneSiteExpectation_Z_field_all_up {{{
+    def test_computeOneSiteExpectation_Z_field_all_up(self,directions=[oneof(0,1)]):
+        system = \
+            System(
+                [1,0],
+                [0,1],
+                buildTensor((2,2,2,2),{
+                    (0,0): Pauli.I,
+                    (0,1): Pauli.Z,
+                    (1,1): Pauli.I,
+                }),
+                array([[[1,0]]]),
+            )
+        for direction in directions:
+            system.contractTowards(direction)
+        self.assertAlmostEqual(len(directions)+1,system.computeExpectation())
     # }}}
 # }}}
 
