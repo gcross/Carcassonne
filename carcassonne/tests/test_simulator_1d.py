2f654bf64853564c47aa524afc91b35da74ee079
Gregory Crosswhite <gcrosswhite@gmail.com>
2013-01-15 15:07:54 +1100

    1D transverse Ising test now uses state difference for sweep convergence.

--- carcassonne/tests/test_simulator_1d.py
+++ carcassonne/tests/test_simulator_1d.py
@@ -66,7 +66,7 @@ class TestSimulator1D(TestCase): # {{{
                 }),
                 ones((1,1,2)),
             )
-        system.sweep_convergence_policy = RelativeOneSiteExpectationDifferenceThresholdConvergencePolicy(1e-7)
+        system.sweep_convergence_policy = RelativeStateDifferenceThresholdConvergencePolicy(1e-5)
         system.run_convergence_policy = RelativeOneSiteExpectationDifferenceThresholdConvergencePolicy(1e-7)
         system.increase_bandwidth_policy = OneDirectionIncrementBandwidthPolicy(0,2)
         system.contraction_policy = RepeatPatternContractionPolicy([0,1])
