446c24b2e08a84fcb104888ef037709e64765a46
Gregory Crosswhite <gcrosswhite@gmail.com>
2013-03-28 17:14:24 +1000

    Now the compression takes into account the operator dimension.

--- carcassonne/compression.py
+++ carcassonne/compression.py
@@ -16,18 +16,22 @@ from .utils import Join, crand, prependDataContractor, unitize
         Join(2,1,4,0), # MRU -- R
         Join(3,1,4,1), # MRU -- R
         Join(1,1,3,0), # MLD -- MRD
+        Join(0,3,4,2), # L -- R
     ],
     [
-        [(0,0),(4,2)],
+        [(0,0),(4,3)],
         [(0,1),(2,0)],
     ]
 )
 def computeProductCompressor(formMatrix,L,R,new_dimension):
-    assert L.shape[1] == L.shape[2]
-    assert R.shape[0] == R.shape[1]
-    assert L.shape[1] == R.shape[1]
+    if L.shape[1] != L.shape[2]:
+        raise ValueError("left inward dimensions do not match (given " + str(L.shape) + ")")
+    if R.shape[0] != R.shape[1]:
+        raise ValueError("right inward dimensions do not match (given " + str(R.shape) + ")")
+    if L.shape[1] != R.shape[1]:
+        raise ValueError("left and right shapes are incompatible (given " + str(L.shape) + " and " + str(R.shape) + ")")
     old_dimension = L.shape[1]
-    b = L.contractWith(R,(1,2),(0,1)).toArray().ravel()
+    b = L.contractWith(R,(1,2,3),(0,1,2)).toArray().ravel()
     compressor = NDArrayData.newRandom(old_dimension,new_dimension).unitize()
     for _ in range(4):
         A = formMatrix(L,compressor.conj(),compressor.transpose().conj(),compressor.transpose(),R).toArray()
--- carcassonne/tests/test_compression.py
+++ carcassonne/tests/test_compression.py
@@ -8,23 +8,23 @@ from . import *
 
 class TestCompression(TestCase): # {{{
     @with_checker # test_computeProductCompressor {{{
-    def test_computeProductCompressor(self,oldplus=irange(0,5),new=irange(1,5),l=irange(1,10),r=irange(1,10)):
+    def test_computeProductCompressor(self,oldplus=irange(0,5),new=irange(1,5),l=irange(1,10),r=irange(1,10),op=irange(1,5)):
         old = new + oldplus
-        Lc1 = NDArrayData.newRandom(l,new,new)
-        Lc1 += Lc1.transpose(0,2,1).conj()
-        L = NDArrayData.newZeros((l,old,old),dtype=complex128)
-        L._arr[:,:new,:new] = Lc1._arr
+        Lc1 = NDArrayData.newRandom(l,new,new,op)
+        Lc1 += Lc1.transpose(0,2,1,3).conj()
+        L = NDArrayData.newZeros((l,old,old,op),dtype=complex128)
+        L._arr[:,:new,:new,:] = Lc1._arr
 
-        Rc1 = NDArrayData.newRandom(new,new,r)
-        Rc1 += Rc1.transpose(1,0,2).conj()
-        R = NDArrayData.newZeros((old,old,r),dtype=complex128)
-        R._arr[:new,:new,:] = Rc1._arr
+        Rc1 = NDArrayData.newRandom(new,new,op,r)
+        Rc1 += Rc1.transpose(1,0,2,3).conj()
+        R = NDArrayData.newZeros((old,old,op,r),dtype=complex128)
+        R._arr[:new,:new,:,:] = Rc1._arr
 
         compressor = computeProductCompressor(L,R,new)
 
         Lc2 = L.absorbMatrixAt(1,compressor).absorbMatrixAt(2,compressor.conj())
         Rc2 = R.absorbMatrixAt(0,compressor.conj()).absorbMatrixAt(1,compressor)
 
-        self.assertDataAlmostEqual(Lc1.contractWith(Rc1,(1,2),(0,1)),Lc2.contractWith(Rc2,(1,2),(0,1)))
+        self.assertDataAlmostEqual(Lc1.contractWith(Rc1,(1,2,3),(0,1,2)),Lc2.contractWith(Rc2,(1,2,3),(0,1,2)))
     # }}}
 # }}}
