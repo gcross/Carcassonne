003845f835f9db96884316db72d604b662dfa79f
Gregory Crosswhite <gcrosswhite@gmail.com>
2013-07-09 14:54:31 +1000

    Resurrecting the 1D2D code.
    I need to reconcile the 1D code with the 2D code
    again so I am resurrecting this code;  it should
    work once more because there are not longer any
    differences in the algorithms used by the 1D code
    and the 2D code.
    
    This reverts commit 5ff666366bd05b2f133a2b4fc3f32c2a0c138905.

---
+++ carcassonne/system/_1d2d.py
@@ -0,0 +1,164 @@
+# Imports {{{
+from copy import copy
+from numpy import complex128, ones, sqrt, zeros
+from numpy.linalg import norm
+
+from . import _1d, _2d
+from .base import BaseSystem
+from ..data import NDArrayData
+from ..sparse import Identity, Complete, TwoSiteOperator 
+from ..utils import Pauli, buildProductTensor, buildTensor
+# }}}
+
+# Classes {{{
+class System(BaseSystem): # {{{
+    @classmethod # new {{{
+    def new(cls,rotation,O=None,OO=None):
+        if OO is None and O is None:
+            raise ValueError("either O or OO must not be None")
+        if OO is None:
+            OO = (0*O,)*2
+        if O is None:
+            O = 0*OO[0]
+        _1d_system = _1d.System(
+            [1,0,0],
+            [0,0,1],
+            buildTensor((3,3,2,2),{
+                (0,0): Pauli.I,
+                (0,2): O,
+                (0,1): OO[1],
+                (1,2): OO[0],
+                (2,2): Pauli.I,
+            }),
+            ones((1,1,2),dtype=complex128)/sqrt(2),
+        )
+
+        if O is not None:
+            O = NDArrayData(O)
+        if OO is not None:
+            OO = NDArrayData(OO)
+
+        if rotation == 0:
+            _2d_system = _2d.System.newTrivialWithSparseOperator(O=O,OO_LR=OO)
+        elif rotation == 1:
+            _2d_system = _2d.System.newTrivialWithSparseOperator(O=O,OO_UD=OO)
+        else:
+            raise ValueError("rotation must be 0 or 1, not {}".format(rotation))
+
+        return cls(rotation,_1d_system,_2d_system)
+    # }}}
+    def __init__(self,rotation,_1d,_2d): # {{{
+        BaseSystem.__init__(self)
+        self.rotation = rotation
+        self._1d = _1d
+        self._2d = _2d
+    # }}}
+    def __copy__(self): # {{{
+        return System(self.rotation,copy(self._1d),copy(self._2d))
+    # }}}
+    def check(self,prefix): # {{{
+        self.checkStates(prefix)
+        self.checkEnvironments(prefix)
+        self.checkNormalization(prefix)
+        self.checkExpectation(prefix)
+        self.checkExpectationMatrix(prefix)
+    # }}}
+    def checkEnvironments(self,prefix): # {{{
+        bandwidth = self._1d.state_center_data.shape[0]
+        _1d_left_environment = self._1d.left_environment.toArray()
+        _2d_left_environment = self.convert2DLeftEnvironment().toArray()
+
+        _1d_right_environment = self._1d.right_environment.toArray()
+        _2d_right_environment = self.convert2DRightEnvironment().toArray()
+        if norm(_1d_right_environment-_2d_right_environment) > 1e-7:
+            raise Exception(prefix + ": for the right environment, norm(_1d-_2d)={} > 1e-7".format(norm(_1d_right_environment-_2d_right_environment)))
+
+        if norm(_1d_left_environment-_2d_left_environment) > 1e-7:
+            raise Exception(prefix + ": for the left environment, norm(_1d-_2d)={} > 1e-7".format(norm(_1d_left_environment-_2d_left_environment)))
+    # }}}
+    def checkExpectation(self,prefix): # {{{
+        _1d_exp = self._1d.computeExpectation()
+        _2d_exp = self._2d.computeExpectation()
+        if abs(_1d_exp-_2d_exp) > 1e-7:
+            raise Exception(prefix + ": for the expectation, abs(1D@{} - 2D@{})={} > 1e-7".format(_1d_exp,_2d_exp,abs(_1d_exp-_2d_exp)))
+    # }}}
+    def checkExpectationMatrix(self,prefix): # {{{
+        _1d = self._1d.formExpectationMatrix().toArray()
+        _2d = self._2d.formExpectationMatrix().toArray().reshape(_1d.shape)
+        if norm(_1d-_2d) > 1e-7:
+            raise Exception(prefix + ": for the expectation matrix, norm(1D 2D)={} > 1e-7".format(norm(_1d-_2d)))
+    # }}}
+    def checkNormalization(self,prefix): # {{{
+        if abs(self._2d.computeNormalization()-1) > 1e-7:
+            raise Exception(prefix + ": the 2D normalization is {} != 1".format(self._2d.computeNormalization()))
+    # }}}
+    def checkStates(self,prefix): # {{{
+        _1d = copy(self._1d.state_center_data.toArray()).real
+        _2d = copy(self._2d.state_center_data.toArray()).reshape(_1d.shape).real
+        if norm(_1d-_2d) > 1e-5:
+            raise Exception(prefix + ": for the center state, norm(_1d-_2d)={} > 1e-5".format(norm(_1d-_2d)))
+    # }}}
+    def computeExpectation(self): #{{{
+        self.check("while computing expectation")
+        return self._1d.computeExpectation()
+    # }}}
+    def computeOneSiteExpectation(self): #{{{
+        _1d = self._1d.computeOneSiteExpectation()
+        _2d = self._2d.computeOneSiteExpectation()
+        if abs(abs(_1d)-abs(_2d)) > 1e-7:
+            raise Exception("for the one-site expectation, abs({}-{})={}>1e-7".format(abs(_1d),abs(_2d),abs(abs(_1d)-abs(_2d))))
+        return _2d
+    # }}}
+    def contractTowards(self,direction): # {{{
+        self.check("before contraction, ")
+        #self._1d.contractTowards(direction)
+        self._2d.contractTowards(2*direction+self.rotation)
+        self.copy2Dto1D()
+        self.check("after contraction, ")
+    # }}}
+    def convert2DLeftEnvironment(self): # {{{
+        slot_of = { 
+            Identity(): 2,
+            TwoSiteOperator(2): 1,
+            Complete(): 0,
+        }
+
+        bandwidth = self._2d.state_center_data.shape[self.rotation]
+        _1d_left_environment = zeros((3,) + (bandwidth,)*2,dtype=complex128)
+        for tag, value in self._2d.sides[self.rotation+2].items():
+            _1d_left_environment[slot_of[tag]] = value.toArray().reshape(bandwidth,bandwidth)
+        return NDArrayData(_1d_left_environment)
+    # }}}
+    def convert2DRightEnvironment(self): # {{{
+        slot_of = { 
+            Identity(): 0,
+            TwoSiteOperator(2): 1,
+            Complete(): 2,
+        }
+
+        bandwidth = self._2d.state_center_data.shape[self.rotation]
+        _1d_right_environment = zeros((3,) + (bandwidth,)*2,dtype=complex128)
+        for tag, value in self._2d.sides[self.rotation].items():
+            _1d_right_environment[slot_of[tag]] = value.toArray().reshape(bandwidth,bandwidth)
+        return NDArrayData(_1d_right_environment)
+    # }}}
+    def copy2Dto1D(self): # {{{
+        self._1d.setCenterState(self._2d.state_center_data.join((0,1),(2,3),4))
+        self._1d.left_environment = self.convert2DLeftEnvironment()
+        self._1d.right_environment = self.convert2DRightEnvironment()
+    # }}}
+    def increaseBandwidth(self,direction,by=None,to=None,do_as_much_as_possible=False): # {{{
+        self.check("before increasing bandwidth")
+        #self._1d.increaseBandwidth(direction,by,to,do_as_much_as_possible)
+        self._2d.increaseBandwidth(direction,by,to,do_as_much_as_possible)
+        self.copy2Dto1D()
+        self.check("after increasing bandwidth")
+    # }}}
+    def minimizeExpectation(self): # {{{
+        self.check("before minimizing, ")
+        self._1d.minimizeExpectation()
+        self._2d.minimizeExpectation()
+        self.check("after minimizing, ")
+    # }}}
+# }}}
+# }}}
