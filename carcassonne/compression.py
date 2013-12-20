d418dfdf42088fa81b3450dc0f971a6b38038747
Gregory Crosswhite <gcrosswhite@gmail.com>
2013-12-20 19:54:26 +1000

    Added another experimental product compressor.

--- carcassonne/compression.py
+++ carcassonne/compression.py
@@ -44,6 +44,42 @@ def computeProductCompressor(formMatrix,L,R,new_dimension):
         compressor = NDArrayData(x.reshape(old_dimension,new_dimension)).unitize()
     return compressor.transpose()
 # }}}
+# def computeCompressor(L,R,new_dimension) {{{
+@prependDataContractor(
+    [
+        Join(0,2,1,5),
+        Join(0,3,1,0),
+        Join(0,4,1,1),
+        Join(0,5,1,2),
+    ],
+    [
+        [(0,0)],
+        [(0,1)],
+        [(1,3)],
+        [(1,4)],
+    ]
+)
+def computeProductCompressor2(formMatrix,left_corner_identity,right_corner_identity,new_dimension):
+    if L.shape[1] != L.shape[2]:
+        raise ValueError("left inward dimensions do not match (given " + str(L.shape) + ")")
+    if R.shape[0] != R.shape[1]:
+        raise ValueError("right inward dimensions do not match (given " + str(R.shape) + ")")
+    if L.shape[1] != R.shape[1]:
+        raise ValueError("left and right shapes are incompatible (given " + str(L.shape) + " and " + str(R.shape) + ")")
+    old_dimension = L.shape[1]
+    b = L.contractWith(R,(1,2,3),(0,1,2)).toArray().ravel()
+    compressor = NDArrayData.newRandom(old_dimension,new_dimension).unitize()
+    for _ in range(4):
+        A = formMatrix(L,compressor.conj(),compressor.transpose().conj(),compressor.transpose(),R).toArray()
+        At = A.transpose().conj()
+        x, info = gmres(
+            LinearOperator((old_dimension*new_dimension,)*2,lambda v: dot(At,dot(A,v)),dtype=complex128),
+            dot(At,b)
+        )
+        assert info == 0
+        compressor = NDArrayData(x.reshape(old_dimension,new_dimension)).unitize()
+    return compressor.transpose()
+# }}}
 # }}}
 
 # Exports {{{
